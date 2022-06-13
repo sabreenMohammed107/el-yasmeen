4200e5330a25a415bbeb1c6d6830c1292bce97c4
sabreenMohammed107 <sabreenm312@gmail.com>
2022-06-13 02:00:01 +0200

    vv

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     /*
@@ -37,5 +38,21 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function logout(Request $request)
+    {
+
+
+            $request->session()->invalidate();
+
+
+            return redirect('/login');
+
+    }
+    protected function authenticated(Request $request, $user)
+    {
+
+        return redirect($redirect);
     }
 }
